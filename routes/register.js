bdff7eb18d9e644dafced55ce6d5de623f11626f
chen <2623023722@qq.com>
2020-11-06 21:20:03 +0800

    后台管理系统

---
+++ routes/register.js
@@ -0,0 +1,46 @@
+var express = require("express");
+//建立一个路由空表！！！！
+var router = express.Router();
+//引入user 模型 类似于英雄联盟  只能有六神装的设计
+const user = require("../sql/user");
+
+
+router.get("/", function (req, res, next) {
+  console.log('此时进入了register')
+  res.render("register");
+});
+
+
+
+router.post("/in", function (req, res, next) {
+  console.log("进入register 的in 处理");
+
+  let obj = req.body;
+  console.log(obj)
+  console.log(obj.username);
+  console.log(obj.password)
+
+  user.findOne({ "username": obj.username }, (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    if (data) {
+      res.redirect("/register");
+    } else {
+      user.insertMany(obj, (err, data) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log(data);
+        if (data) {
+          res.redirect("/login");
+        } else {
+          res.redirect("/register");
+        }
+      });
+    }
+  });
+});
+
+
+module.exports = router;
